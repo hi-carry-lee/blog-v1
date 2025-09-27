53cbb792f40ee5a401aaaaadaa6cac128a83232c
liren <327381633@qq.com>
2025-09-27 22:53:09 +0800

    add basic configuration of authjs

---
+++ auth.ts
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  session: {
+    strategy: "jwt" as const,
+  },
+  pages: {
+    signIn: "/sign-in", // *it's a url, so it must start with "/"
+    error: "/sign-in",
+  },
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        return null;
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user, trigger, session }) {
+      return token;
+    },
+    session({ session, trigger, token }) {
+      return session;
+    },
+  },
+});
---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { auth } from "@/auth";
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+  const isApiAuthRoute = nextUrl.pathname.startsWith("/api");
+  const isPublicRoute = ["/auth/login", "/auth/register"].includes(
+    nextUrl.pathname
+  );
+  const isAuthRoute = ["/auth/login", "/auth/register"].includes(
+    nextUrl.pathname
+  );
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return NextResponse.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
